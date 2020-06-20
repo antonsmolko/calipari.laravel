2e4623de493f314c2066b007d3a19c1ebb6f8cb7
Anton Smolko <asmolko@yandex.ru>
2020-06-20 16:48:03 +0300

    Update index.blade

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,14 +3,13 @@
 @section('page_title', 'Главная')
 
 @section('content')
-    <?php echo phpinfo(); ?>
-{{--    <hero-home></hero-home>--}}
-{{--    <albums-home></albums-home>--}}
-{{--    <materials-home></materials-home>--}}
-{{--    <chronometer-home></chronometer-home>--}}
-{{--    <interiors-home></interiors-home>--}}
-{{--    <consultation-component></consultation-component>--}}
-{{--    <topics-home></topics-home>--}}
-{{--    <you-get-home></you-get-home>--}}
-{{--    <buying-scheme-home></buying-scheme-home>--}}
+    <hero-home></hero-home>
+    <albums-home></albums-home>
+    <materials-home></materials-home>
+    <chronometer-home></chronometer-home>
+    <interiors-home></interiors-home>
+    <consultation-component></consultation-component>
+    <topics-home></topics-home>
+    <you-get-home></you-get-home>
+    <buying-scheme-home></buying-scheme-home>
 @endsection
