fe08aa66a25701c277029d438b32d29f87ca535f
Anton Smolko <asmolko@yandex.ru>
2020-07-24 16:00:49 +0300

    Update OrderHasBeenPaid.php: minor bugs fixed

--- app/Notifications/OrderHasBeenPaid.php
+++ app/Notifications/OrderHasBeenPaid.php
@@ -8,6 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Messages\SlackMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\Str;
 
 class OrderHasBeenPaid extends Notification
 {
@@ -51,16 +52,17 @@ class OrderHasBeenPaid extends Notification
             ->to('#orders')
             ->content( $paymentData['description'])
             ->attachment(function ($attachment) use ($paymentData) {
-                $cardRegExp = "/[0-9*]{4}/g";
-                $cardPattern = '${1} ${2} ${3} ${4}';
+                $cardRegExp = '/[0-9*]{4}/';
                 $cardNumber = $paymentData['payment_method']['card']['first6'] . '******' . $paymentData['payment_method']['card']['last4'];
-                $formatCardNumber = preg_replace($cardRegExp, $cardPattern, $cardNumber);
+                $formattedCardNumber = Str::of($cardNumber)
+                    ->matchAll($cardRegExp)
+                    ->join(' ');
                 /** Не форматировать. Оставить так. !!! */
                 $attachment
                     ->content(
                         '*ID платежа* - ' . $paymentData['id'] . '
 *Сумма* - ' . $paymentData['amount']['value'] . ' ₽
-*Карта: номер* - ' . $formatCardNumber . '
+*Карта: номер* - ' . $formattedCardNumber . '
 *Карта: месяц/год* - ' . $paymentData['payment_method']['card']['expiry_month'] . '/' . $paymentData['payment_method']['card']['expiry_year'] . '
 *Карта: платежная система* - ' . $paymentData['payment_method']['card']['card_type'])
                     ->markdown(['text']);
