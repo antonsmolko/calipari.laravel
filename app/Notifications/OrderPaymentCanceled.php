6eb87082ca350d1e9e07729f714ea3e856af54f0
Anton Smolko <asmolko@yandex.ru>
2020-07-27 22:56:22 +0300

    OrderPaymentCanceled bugfixed: 59 [order_number] replaced by

--- app/Notifications/OrderPaymentCanceled.php
+++ app/Notifications/OrderPaymentCanceled.php
@@ -56,7 +56,7 @@ class OrderPaymentCanceled extends Notification
         return (new SlackMessage)
             ->from('calipari.ru', ':x:')
             ->to('#payment')
-            ->content('Оплата заказа № ' . $paymentReport['metadata']['order_number'] . ' отменена!')
+            ->content('Оплата заказа № ' . $paymentReport['order_number'] . ' отменена!')
             ->attachment(function ($attachment) use ($content) {
                 $attachment->content($content)->markdown(['text']);
             });
