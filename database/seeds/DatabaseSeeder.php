b61f67816678e2c9aa065ceb789f229db99a1de3
Anton Smolko <asmolko@yandex.ru>
2020-10-08 12:07:09 +0300

    change: remove from index seed_data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,22 +12,22 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-//        $filePath = base_path(config('seeds.seeds_data_path')) . 'csv/calipari.topics.csv';
-//        if (File::exists($filePath) && File::isReadable($filePath) && File::isFile($filePath)) {
-//            $csv = array_map('str_getcsv', file($filePath));
-//            array_walk($csv, function(&$a) use ($csv) {
-//                $a = array_combine($csv[0], $a);
-//            });
-//            array_shift($csv);
-//            dd($csv);
-//        }
-//        dd('stop');
+        $filePath = base_path(config('seed_settings.seeds_data_path')) . 'csv/calipari.categories.csv';
+        if (File::exists($filePath) && File::isReadable($filePath) && File::isFile($filePath)) {
+            $csv = array_map('str_getcsv', file($filePath));
+            array_walk($csv, function(&$a) use ($csv) {
+                $a = array_combine($csv[0], $a);
+            });
+            array_shift($csv);
+            dd($csv);
+        }
+        dd('stop');
 
-//        $csv = array_map('str_getcsv', file($file));
-//        array_walk($csv, function(&$a) use ($csv) {
-//            $a = array_combine($csv[0], $a);
-//        });
-//        array_shift($csv);
+        $csv = array_map('str_getcsv', file($file));
+        array_walk($csv, function(&$a) use ($csv) {
+            $a = array_combine($csv[0], $a);
+        });
+        array_shift($csv);
 
         $this->call([
             LaratrustSeeder::class,
